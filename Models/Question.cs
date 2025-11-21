3ab33ca733ec329b74bb910c10ca0e5939b2e55f
MRtim <timatosalikaze@gmail.com>
2025-11-21 23:15:16 +0500

    все работает

--- Models/Question.cs
+++ Models/Question.cs
@@ -6,16 +6,17 @@ namespace TestingPlatform.Domain.Models
     public class Question
     {
         public int Id { get; set; }
-        public required string Text { get; set; }
-        public required int Number { get; set; }
-
+        public string Text { get; set; }
+        public int Number { get; set; }
         [MaxLength(2000)]
-        public string? Description { get; set; }
-        public required AnswerType AnswerType { get; set; }
-        public bool IsScoring { get; set; } = true;
+        public string Description { get; set; }
+        public AnswerType AnswerType { get; set; }
+        public bool IsScoring { get; set; }
         public int? MaxScore { get; set; }
-        public required int TestId { get; set; }
 
+
+
+        public required int TestId { get; set; }
         public Test? Test { get; set; }
         public List<UserAttemptAnswer>? UserAttemptAnswers { get; set; }
         public List<Answer> Answers { get; set; } = new List<Answer>();
