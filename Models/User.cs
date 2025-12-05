86f9a63cf1cb460f07e77d1a457725f43df925fa
MRtim <timatosalikaze@gmail.com>
2025-12-05 23:25:52 +0500

    сделана практика 20

--- Models/User.cs
+++ Models/User.cs
@@ -1,24 +1,16 @@
-﻿using System.Text.Json.Serialization;
-using System.ComponentModel.DataAnnotations;
-using TestingPlatform.Domain.Enums;
+﻿using TestingPlatform.Domain.Enums;
 
-namespace TestingPlatform.Domain.Models
+namespace TestingPlatform.Domain.Models;
+public class User
 {
-    public class User
-    {
-        public int Id { get; set; }
-        public string? Login { get; set; }
-        public required string PasswordHash { get; set; }
-        public required string Email { get; set; }
-        public required string FirstName { get; set; }
-        public string? MiddleName { get; set; }
-        public required string LastName { get; set; }
-        public UserRole Role { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
-        [JsonIgnore]
-        public Student? Student { get; set; }
-    }
+    public int Id { get; set; }
+    public string Login { get; set; }
+    public string PasswordHash { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public string LastName { get; set; }
+    public UserRole Role { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public Student? Student { get; set; }
 }
-
-
